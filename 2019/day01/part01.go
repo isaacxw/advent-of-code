5c0cd4d75e00ea8eef6a90fe06b216f30711be70
Isaac <isaactxw@gmail.com>
2019-12-02 19:29:45 -0800

    Day 1 solutions

---
+++ 2019/day01/part01.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	file, err := os.Open("input01")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	defer file.Close()
+
+	var modules []int
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		weight, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		modules = append(modules, weight)
+	}
+
+	var totalFuel int 
+
+	for _, module := range modules {
+		fuel := module / 3 - 2
+		totalFuel += fuel
+	}
+
+	fmt.Println(totalFuel)
+}
---
+++ 2019/day01/part02.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	file, err := os.Open("input01")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	defer file.Close()
+
+	var modules []int 
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		weight, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		modules = append(modules, weight)
+
+	}
+
+	var totalFuel int
+
+	for i := 0; i < len(modules); i++ {
+		fuel := modules[i] / 3
+
+		if (fuel - 2) > 0 {
+			fuel = fuel - 2
+			totalFuel += fuel
+			modules = append(modules, fuel)
+		}
+	}
+
+	fmt.Println("Total fuel: " + strconv.Itoa(totalFuel))
+}
