1b69d1b923315204fb6b611f4550ea7321bca13a
Isaac <isaactxw@gmail.com>
2020-12-01 21:34:35 -0800

    Add Readme

--- 2020/day1/solution.js
+++ 2020/day1/solution.js
@@ -9,14 +9,13 @@ const part1 = (input) => {
             return a * b;
         }
     }
-
     return null;
 }
 
 const part2 = (input) => {
     for (let i = 0; i < input.length; i++) {
         for (let j = 1; j < input.length; j++) {
-            for (let k = 0; k < input.length; k++) {
+            for (let k = 2; k < input.length; k++) {
                 if (input[i] + input[j] + input[k] === 2020) {
                     return input[i] * input[j] * input[k];
                 }
--- 2020/generator.js
+++ 2020/generator.js
@@ -3,17 +3,14 @@ const fs = require("fs");
 const readFile = util.promisify(fs.readFile);
 
 // Gets the input file based on the module using this function.
-// @returns string[] of lines
 async function getInput() {
-  // const modulePath = module.parent.filename;
   const inputPath = 'input.txt';
 
   try {
     const file = await readFile(inputPath);
-
     return file.toString().split("\n");
-  } catch (e) {
-    console.error('No input file found. Please add one.');
+  } catch (err) {
+    console.error('No input file found.');
     process.exit(1);
   }
 }
